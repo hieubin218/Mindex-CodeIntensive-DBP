3f42ba47eb04c43de5598356056e9bce38a268c5
Hieu Dang <76166385+hieubin218@users.noreply.github.com>
2021-08-11 20:34:08 +0700

    Add files via upload

--- src/SmartPhoneStore/ProductDetail.js
+++ src/SmartPhoneStore/ProductDetail.js
@@ -1,1 +1,59 @@
-adfasdfad;
+import React, { Component } from 'react';
+
+class ProductDetail extends Component {
+    render() {
+        return (
+            <div className="mt-5">
+                <h3>Product Detail</h3>
+                <div className="row">
+                    <div className="col-md-4">
+                        <h4>Phone</h4>
+                        <img src="..." className="card-img-top" />;
+                    </div>
+
+                    <div className="col-md-8">
+                        <div className="table">
+                            <thead>
+                                <tr colSpan="2">
+                                    <th className="text-center mb-2">Thông Số Kỹ Thuật</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Màn hình</td>
+                                    <td>Hi</td>
+                                </tr>
+                                <tr>
+                                    <td>Hệ điều hành</td>
+                                    <td>Hi</td>
+                                </tr>
+                                <tr>
+                                    <td>Camera Trước</td>
+                                    <td>h</td>
+                                </tr>
+                                <tr>
+                                    <td>Camera Sau</td>
+                                    <td>h</td>
+                                </tr>
+                                <tr>
+                                    <td>RAM</td>
+                                    <td>h</td>
+                                </tr>
+                                <tr>
+                                    <td>ROM</td>
+                                    <td>h</td>
+                                </tr>
+                                
+                            </tbody>
+                        
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            
+        );
+    }
+}
+
+export default ProductDetail;
--- src/SmartPhoneStore/ProductList.js
+++ src/SmartPhoneStore/ProductList.js
@@ -1,1 +1,40 @@
-hello 
+import React, { Component } from 'react';
+import ProductItem from './ProductItem';
+
+
+
+export default class ProductList extends Component {
+    render() {
+        // Nhận props data của Component Cha truyền xuống là SmartPhoneStore
+        const {phoneProducts, onSelectedProduct} = this.props;
+        return (
+            <div className="row">
+                <h1>Product List</h1>
+                {
+                    phoneProducts.map((productItem, index) => {
+                        return (
+                            <div className="col-4 col-md-4">
+                                {/*Truyền props onSelectedProduct và product xuống ProductItem */}
+                                <ProductItem 
+                                    onSelectedProduct={onSelectedProduct}
+                                    product={productItem} 
+                                    key={index} />
+                            </div>
+                        ); 
+                    })
+                };
+            </div>
+        );
+    }
+}
+
+
+// grid system on Boostrap
+// Nguyên màn hình có 12 cột, col-4 = mỗi item có 4 cột 
+// --> muốn set up items thành 1 hàng thì bọc child div vào div cha với className="row"
+/**
+ * <div class="row">
+ *      <div class="col-4">
+ *          ......
+ *      </div>
+ */
