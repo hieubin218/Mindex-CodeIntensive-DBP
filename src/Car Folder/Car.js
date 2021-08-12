9a32a865a96f2fa76198f6cd5afddda3f23d1d94
Hieu Dang <76166385+hieubin218@users.noreply.github.com>
2021-08-12 09:48:39 +0700

    Add files via upload

--- src/Car Folder/Car.js
+++ src/Car Folder/Car.js
@@ -1,46 +1,54 @@
 import React, {Component} from "react";
 import "./Car.css";
 
-
+/**
+ * Store data (img url) into constructor as default state
+ * Change url when button clicked
+ */
+ 
 export default class Car extends Component {
     constructor(props) {
         super(props);
-        this.state={
-            selectedCar: "assets/img/red-car.png",
-        };
+        this.state = {
+            selectedCar: "Assets-Image/red-car.png",
+        }
     }
 
     changeColor = (color) => {
-        this.setState({
+        this.setState( {
             selectedCar: color,
         });
-    };
-
+    }
 
     render() {
         return (
             <div className="car">
                 <div className="car_left">
-                    <h2> Please choose your car's color</h2>
+                    <h2> Please Choose your favorite Car's color</h2>
                     <img src={this.state.selectedCar} />
                 </div>
 
                 <div className="car_right">
-                    <button 
-                        class="button red" 
-                        onClick={() => this.changeColor("assets/img/red-car.png")}>
-                        Red
+                    <button
+                        class="button red"
+                        onClick={() => this.changeColor("Assets-Image/red-car.png")}>
+                            Red
                     </button>
-                    <button 
-                        class="button black"
-                        onClick= {() => this.changeColor("assets/img/black-car.png")}>
-                        Black
+
+                    <button
+                        className="button black"
+                        onClick={() => this.changeColor("Assets-Image/black-car.png")}>
+                            Black
                     </button>
-                    <button class="button white"
-                        onClick= {() => this.changeColor("assets/img/black-car.png")}>
-                        White
+
+                    <button
+                        className="button white"
+                        onClick={() => this.changeColor("Assets-Image/white-car.png")}>
+                            White
                     </button>
+
                 </div>
+
             </div>
         )
     }
