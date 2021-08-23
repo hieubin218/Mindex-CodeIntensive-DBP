88539552d0f36d070f8a241203ce5d277bcf018f
Hieu Dang <76166385+hieubin218@users.noreply.github.com>
2021-08-23 09:20:08 +0700

    Add files via upload

--- src/BurgerKing Checkout Component/BurgerCheckOut SubComponent/BurgerCheckOut.jsx
+++ src/BurgerKing Checkout Component/BurgerCheckOut SubComponent/BurgerCheckOut.jsx
@@ -2,64 +2,79 @@ import React, { Component } from 'react';
 import './BurgerCheckOut.css';
 
 
-class BurgerCheckOut extends Component {
+export default class BurgerCheckOut extends Component {
+
     render() {
-        const {menu, onHandleAmountIngredient, onResetOrder} = this.props;
+        // Receive props from parents Component
+        const {menu, onHandleAmountIngredient, onResetOrder, makeOrder, onMakeOrder} = this.props;
+        const {toppingKey} = this.props.menu;
+
+        // Method: Calculate Total Price
+        let totalPrice = 0;
+        for (let item in menu) {
+            // totalPrice += item[toppingKey] * makeOrder[toppingKey]
+            console.log(makeOrder)
+            console.log(makeOrder[toppingKey])
+            
+        }
+
+
+
         return (
-           <div className="menu">
+            <div className="menu">
                <h4 className="text-center pd-3">Custom your Burger</h4>
                <hr></hr>
-               <table className="table text-center">
-                <thead className="fw-bold">
-                    <tr>
-                        <td>TOPPING</td>
-                        <td>OPTION</td>
-                        <td>UNIT PRICE</td>
-                        <td>PRICE</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        menu.map((topping) => {
-                            const {name, price, foodID} = topping;
-                            return (
-                            
-                                    <tr>
-                                        <td className="salad-Topping fw-bold">{name}</td>
-                                        <td>
-                                            <button className="mx-2 less"
-                                                onClick={() => onHandleAmountIngredient(foodID, -1)}>
-                                                    LESS
-                                            </button>
-                                            <span>1</span>
-                                            <button className="mx-2 more"
-                                                onClick={() => onHandleAmountIngredient(foodID, +1)}>
-                                                    MORE
-                                            </button>
-                                        </td>
-                                        <td className="unitPrice">${price}</td>
-                                        <td className="Price">20</td>
-                                    </tr>
-                               
+                <table className="table text-center">
+                    <thead className="fw-bold">
+                        <tr>
+                            <td>TOPPING</td>
+                            <td>OPTION</td>
+                            <td>UNIT PRICE</td>
+                            <td>PRICE</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            menu.map((toppingType, index) => {
+                                const {name, price, toppingKey} = toppingType;
+                                return (
+                                
+                                        <tr key={index}>
+                                            <td className="salad-Topping fw-bold">{name}</td>
+                                            <td>
+                                                <button className="mx-2"
+                                                    // toppingKey [salad, cheese, meat, bacon] và value -1 (less button -1 in amount )
+                                                    onClick={() => onHandleAmountIngredient(toppingKey, -1)}
+                                                    disabled={makeOrder[toppingKey] === 0}>
+                                                        LESS
+                                                </button>
 
-                                )
-                        })
-                    }
-                </tbody>
-                <tr className="fw-bold">
-                    <td>Total Cost</td>
-                    <td>$</td>
-                </tr>
+                                                <span>{makeOrder[toppingKey]}</span>
 
-                <tr>
-                    <button className="mx-3 mt-3">ORDER NOW</button>
+                                                <button className="mx-2"
+                                                    // toppingKey [salad, cheese, meat, bacon] và value +1 (more button +1 in amount )
+                                                    onClick={() => onHandleAmountIngredient(toppingKey, +1)}>
+                                                        MORE
+                                                </button>
+                                            </td>
+                                            <td className="unitPrice">${price}</td>
+                                            {/* <td className="Price">$ {price * makeOrder[toppingKey] }</td> */}
+                                            <td className="Price">$ {price * makeOrder[toppingKey]}</td>
+                                        </tr>
+                                    );
+                            })}
+                        <tr className="fw-bold">
+                            <td>Total Cost</td>
+                            <td>$ {totalPrice}</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <div>
+                    <button color="var(--bg-color-primary)" className="mx-3 mt-3" onClick={() => onMakeOrder()}>ORDER NOW</button>
                     <button className="mx-3" onClick={() => onResetOrder()}>Reset</button>
-                </tr>
-
-               </table>
+                </div>
            </div>
-        );
+        )
     }
-}
-
-export default BurgerCheckOut;
+}   
