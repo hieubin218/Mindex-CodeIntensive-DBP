7c11d227fbed1dea06a0f3ae89c98376ee522e10
Hieu Dang <76166385+hieubin218@users.noreply.github.com>
2021-09-01 11:34:04 +0700

    Add files via upload

---
+++ src/Components/UsersList/UsersList.js
@@ -0,0 +1,25 @@
+import React, {Component} from "react";
+import UserItem from "../UserItem/UserItem";
+
+// Danh sách những user item
+// nhận list từ Home Component and map 
+// Xây Dựng user component item
+
+class UsersList extends Component {
+    render() {
+        const {users} = this.props;
+        return (
+            <div className="row">
+                {
+                    users.map((userItem, index) => {
+                        return (
+                            <UserItem userItem={userItem} key={index} />
+                        )
+                    })
+
+                }
+            </div>
+        )
+    }
+}
+export default UsersList;
