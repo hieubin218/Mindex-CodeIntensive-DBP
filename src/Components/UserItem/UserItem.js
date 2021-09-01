a1d38d846ebcd9c2165a5cbcd6e580bb20472ab7
Hieu Dang <76166385+hieubin218@users.noreply.github.com>
2021-09-01 21:09:20 +0700

    Add files via upload

--- src/Components/UserItem/UserItem.js
+++ src/Components/UserItem/UserItem.js
@@ -1,20 +1,33 @@
-import React from 'react';
-import "./UserItem.css";
+import React from "react"
+import "./UserItem.css"
+import { useHistory } from "react-router-dom"
 
 
 const UserItem = (props) => {
-    const {login, avatar_url } = props.userItem;
+    const {login, avatar_url} = props.userItem;
+
+    const history = useHistory();
+    const onLinkToUserDetail = () => {
+        // props: history là React-Router-DOM cung cấp
+        history.push(`/users/${login}`);
+    };
+
+
     return (
         <div className="user col-12 col-md-6 col-lg-4">
-            <div className="user_avatar">
+            <div className="user__avatar">
                 <img src={avatar_url} alt="img" />
             </div> 
 
             <p className="user_name">{login}</p>
-            <button className="btn btn-secondary">MORE</button>
+            <button 
+                className="btn btn-secondary"
+                onClick={onLinkToUserDetail} >
+                MORE
+            </button>
             
         </div>
     );
 
 }
-export default UserItem;
+export default UserItem
