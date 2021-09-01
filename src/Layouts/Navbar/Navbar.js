a166d9f14a4eb75f2592957ded27ed265eb726a2
Hieu Dang <76166385+hieubin218@users.noreply.github.com>
2021-09-01 11:38:09 +0700

    Add files via upload

---
+++ src/Layouts/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+// Navbar not use State, use Component function
+import React from "react";
+import {Link} from "react-router-dom";
+import "./Navbar.css";
+
+
+// Design Navbar at top of page
+// <Link> giống thẻ <a>
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <p className="navbar_title">Github Finder</p>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+            </ul>
+
+        </div>
+    )
+};
+
+export default Navbar;
+
