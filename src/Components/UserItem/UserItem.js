989d05eee4eee78909e288da3f3b6ff9f2dadf2a
Hieu Dang <76166385+hieubin218@users.noreply.github.com>
2021-09-04 18:05:55 +0700

    Add files via upload

--- src/Components/UserItem/UserItem.js
+++ src/Components/UserItem/UserItem.js
@@ -1,12 +1,17 @@
 import React from "react"
 import "./UserItem.css"
+
+// React Hook - use in Funtional (not Class, not this)
 import { useHistory } from "react-router-dom"
 
 
 const UserItem = (props) => {
     const {login, avatar_url} = props.userItem;
 
+
+    // history is an object {location, action, push, replace,...}
     const history = useHistory();
+
     const onLinkToUserDetail = () => {
         // props: history là React-Router-DOM cung cấp
         history.push(`/users/${login}`);
