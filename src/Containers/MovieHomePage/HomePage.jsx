647569ef0547385d9f179df20ade893bbc2eb41b
Hieu Dang <76166385+hieubin218@users.noreply.github.com>
2021-09-17 21:45:07 +0700

    Add files via upload

--- src/Containers/MovieHomePage/HomePage.jsx
+++ src/Containers/MovieHomePage/HomePage.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import Carousel  from '../../Components/Carousel/Carousel';
 import MovieContainer from '../../Components/MovieContainer/MovieContainer';
@@ -5,12 +6,65 @@ import SearchMovie from "../../Components/SearchBar/SearchMovie";
 
 
 class HomePage extends Component {
+    constructor(props) {
+        super(props); 
+        this.state={
+            movies: [],
+        };
+    };
+
+    onFetchPopularMovies = async() => {
+        try {
+            const response = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=a32287ddc2296da6ef51bbca9953037d")
+            this.setState({
+                movies: response.data.results,
+            })
+        
+        } catch (error) {
+            throw(error);
+        }
+    }
+
+
+    onFetchUpComingMovies = async() => {
+        try {
+            const response = await axios.get("https://api.themoviedb.org/3/movie/upcoming?api_key=a32287ddc2296da6ef51bbca9953037d")
+            this.setState({
+                movies: response.data.results,
+            })
+        
+        } catch (error) {
+            throw(error);
+        }
+    }
+
+
+    onFetchTopRatedMovies = async() => {
+        try {
+            const response = await axios.get("https://api.themoviedb.org/3/movie/top_rated?api_key=a32287ddc2296da6ef51bbca9953037d")
+            this.setState({
+                movies: response.data.results,
+            })
+        
+        } catch (error) {
+            throw(error);
+        }
+    }
+
+
+
+
+
     render() {
         return (
             <div>
                 <Carousel />
                 <SearchMovie />
-                <MovieContainer />
+                <MovieContainer movies={this.state.movies}  
+                                onFetchPopularMovies={this.onFetchPopularMovies}
+                                onFetchUpComingMovies={this.onFetchUpComingMovies}
+                                onFetchTopRatedMovies={this.onFetchTopRatedMovies}
+                                />
 
                 
             </div>
