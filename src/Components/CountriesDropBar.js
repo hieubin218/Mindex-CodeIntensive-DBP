d249ef3ac6ca70b02f8b7f1cbcf6a852a675f14a
Hieu Dang <76166385+hieubin218@users.noreply.github.com>
2021-09-19 10:48:42 +0700

    Add files via upload

---
+++ src/Components/CountriesDropBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const CountriesDropBar = (props) => {
+   
+
+    return (
+        <div>
+            <select
+                    placehoder="Select a country"
+                    defaultValue="global"
+                    >
+                    
+                    <option>
+
+                    </option>
+
+            </select>
+
+
+        </div>
+    )
+}
+
+export default CountriesDropBar;
