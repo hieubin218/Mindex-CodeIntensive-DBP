3ba7b553c884c5c95fcdb7293e5fcc88e5b96de9
Hieu Dang <76166385+hieubin218@users.noreply.github.com>
2021-09-22 18:08:22 +0700

    Add files via upload

---
+++ src/Components/SummaryChartMap/CovidLineChart.js
@@ -0,0 +1,82 @@
+import HighchartsReact from "highcharts-react-official";
+import React, {useState, useEffect} from "react";
+import Highchart from "highcharts";
+
+
+const generatedOptions = (data) => {
+    const categories = [];
+    return {
+        // Height of line chart
+        chart: {
+            height: 500,
+        },
+        // Title of Line Chart
+        title: {
+            text: "Total Confirmed Cases",
+        },
+        // Horizontal line -> for date by date
+        xAis: {
+            categories: categories,
+            crosshair: true,
+        },
+        // color of line 
+        colors: ["#53585B"],
+        // Vertical Line -> null
+        yAxis: {
+            min: 0,
+            title: {
+                text: null,
+            },
+        },
+        // cấu hình nội dung của line chart tại từng thời điểm on line (date, total cases)
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span>',
+            pointFormat:
+                '<tr> <td></td>' +
+                '<td></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true,
+        },
+        // Quan trọng: Dữ liệu để truyền trên Line Chart 
+        series: [
+            {
+                name: "Total Cases",
+                data: data.map((item) => item.Confirmed),
+            },
+        ]
+
+    }
+
+}
+
+
+// data props from component cha
+// when data modify, run again generateOptions --> to generate new option
+function CovidLineChart({data}) {
+    const [options, setOptions] = useState({});
+
+    // "data" in dependency --> when "data" change, useEffect will also change with new "data";
+    useEffect(() => {
+        setOptions(generatedOptions(data));
+    }, [data])
+
+
+    return (
+        <div>
+            <HighchartsReact highcharts={Highchart}
+                             options={{options}}>
+
+            </HighchartsReact>
+           
+
+        </div>
+    )
+};
+
+export default CovidLineChart;
+
+/** NOTE:
+ * <HighchartsReact option={}> 
+ * --> dữ liệu option sẽ bị thay đổi về country khác --> phải tạo lại một option mới về country mới 
+ */
---
+++ src/Components/SummaryChartMap/CovidMap.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+function CovidMap() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+
+export default CovidMap;
